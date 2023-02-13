36ff8a22c07cbfb2f71a6b82b26b68880e4e600f
GabiKovtun <gabi.tovt@gmail.com>
2023-02-13 13:52:06 +0100

    add task 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+const  {elements: {email, password},}  = event.currentTarget; 
+
+
+  if (email.value === '' || password.value === '') {
+    alert('Bсі поля повинні бути заповнені!');
+    return;
+  }
+
+  
+  console.log( `email: ${email.value}`);
+  console.log( `pass: ${password.value}`);
+  form.reset();
+});
