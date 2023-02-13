3f08d3728d331aeda81b83818fd018971a1284c3
GabiKovtun <gabi.tovt@gmail.com>
2023-02-13 14:04:09 +0100

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const bodyEl = document.querySelector('body');
+const colorEl = document.querySelector('.color');
+const colorBtn = document.querySelector('.change-color');
+
+colorBtn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorEl.textContent = color;
+});
